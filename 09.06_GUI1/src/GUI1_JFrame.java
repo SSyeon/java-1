aab2302eddb75fc72e18ae5ba6f9e73fd654d964
ssyeon <tjdusdlfkrhd@naver.com>
2019-09-06 09:07:50 +0900

    어쩌다 윈도우+label

--- 09.06_GUI1/src/GUI1_JFrame.java
+++ 09.06_GUI1/src/GUI1_JFrame.java
@@ -3,7 +3,9 @@ import javax.swing.*;
 public class GUI1_JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창 이름");
+        JLabel label = new JLabel("드디어 금요일이당!!!");
 
+        frame.add(label);
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

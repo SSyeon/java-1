61d4a7081044615756e6582b446ceae978a0766e
ssyeon <tjdusdlfkrhd@naver.com>
2019-09-11 08:46:22 +0900

    frame, panel, label, button

--- 09.06_GUI1/src/GUI1_JFrame.java
+++ 09.06_GUI1/src/GUI1_JFrame.java
@@ -3,9 +3,16 @@ import javax.swing.*;
 public class GUI1_JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창 이름");
-        JLabel label = new JLabel("드디어 금요일이당!!!");
+        JPanel panel = new JPanel();
 
-        frame.add(label);
+        JLabel label = new JLabel("드디어 금요일이당!!!");
+        JButton btn = new JButton("오예");
+
+        panel.add(label);
+        panel.add(btn);
+
+        frame.add(panel);
+
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
